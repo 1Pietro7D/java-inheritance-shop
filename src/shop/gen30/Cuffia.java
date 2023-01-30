9bc6c39b43cdc5a3a061a9bd25089f9ff4b2ff38
PietroDanieli <1pie7d@gmail.com>
2023-01-30 17:03:08 +0100

    add subclass Cuffia

--- src/shop/gen30/Cuffia.java
+++ src/shop/gen30/Cuffia.java
@@ -1,5 +1,30 @@
 package shop.gen30;
 
 public class Cuffia extends Prodotto {
+	private String color;
+	private boolean wireless;
+
+	public Cuffia(String name, String brand, String color, boolean wireless) {
+		super(name, brand);
+		this.setColor(color);
+		this.setWireless(wireless);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public boolean isWireless() {
+		return wireless;
+	}
+
+	public void setWireless(boolean wireless) {
+		this.wireless = wireless;
+	}
 
 }
